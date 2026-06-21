966b463caaac5b5da413068e3839db5b9c7c7bd7
SystematicError <systematicerror@users.noreply.github.com>
2026-06-21 12:19:51 +0100

    refactor: separate piece value function

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -1,15 +1,29 @@
 use cozy_chess::{Board, Color, Piece};
 
+#[inline(always)]
+pub fn piece_value(piece: Piece) -> i32 {
+    match piece {
+        Piece::Queen => 900,
+        Piece::Rook => 500,
+        Piece::Knight => 300,
+        Piece::Bishop => 300,
+        Piece::Pawn => 100,
+        Piece::King => 0,
+    }
+}
+
 pub fn evaluate(board: &Board) -> i32 {
     let mut material_score = 0;
 
-    for (piece, value) in [
-        (Piece::Queen, 900),
-        (Piece::Rook, 500),
-        (Piece::Knight, 300),
-        (Piece::Bishop, 300),
-        (Piece::Pawn, 100),
+    for piece in [
+        Piece::Queen,
+        Piece::Rook,
+        Piece::Knight,
+        Piece::Bishop,
+        Piece::Pawn,
     ] {
+        let value = piece_value(piece);
+
         let white_count = board.colored_pieces(Color::White, piece).len() as i32;
         let black_count = board.colored_pieces(Color::Black, piece).len() as i32;
 
