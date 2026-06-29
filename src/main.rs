27e4367d807d23494b9be63662ba27433065d0b3
SystematicError <systematicerror@users.noreply.github.com>
2026-06-29 01:13:51 +0100

    feat: basic uci parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+mod position;
+mod uci;
+
+use crate::uci::Uci;
+use std::io::{BufRead, stdin};
+use std::process::exit;
+
 fn main() {
-    todo!();
+    for line in stdin().lock().lines() {
+        if let Some(command) = Uci::parse(&line.unwrap()) {
+            match command {
+                Uci::Uci => {
+                    println!("id name Updog");
+                    println!("id author SystematicError");
+                    println!("option name Threads type spin default 1 min 1 max 1");
+                    println!("option name Hash type spin default 0 min 0 max 0");
+                    println!("uciok");
+                }
+
+                Uci::IsReady => println!("readyok"),
+
+                Uci::SetOption(_name, _value) => todo!(),
+
+                Uci::Position(_position) => todo!(),
+
+                Uci::Go => todo!(),
+
+                Uci::Quit => exit(0),
+            }
+        }
+    }
 }
